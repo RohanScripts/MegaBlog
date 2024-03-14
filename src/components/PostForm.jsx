4754f9450360b9ea7a373a50a58e927a1ec4a65d
RohanScripts <shelarrohan1810@gmail.com>
2024-03-15 00:28:36 +0530

    files updated

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,7 +3,7 @@ import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import service from "../appwrite/Config";
 import { useNavigate } from "react-router-dom";
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function PostForm({ post }) {
   const { register, handleSubmit, watch, setValue, control, getValues } =
